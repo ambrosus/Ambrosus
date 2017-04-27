e86b0015ad111f297c572b61252b92f2a4b13831
x <matthew@roberts.pm>
2017-04-27 13:01:31 +1000

    Fix tests and remove debug stuff from contracts.

--- contracts/DeliveryContract.sol
+++ contracts/DeliveryContract.sol
@@ -47,7 +47,6 @@ contract DeliveryContract is Assertive {
     uint accepted_no;
 
     Stages public stage = Stages.New;
-    bool public state_machine_is_active;
 
     Attribute [] public attributes;
 
@@ -64,105 +63,93 @@ contract DeliveryContract is Assertive {
     }
     
     modifier onlyStage(Stages desired_stage) {
-      if(state_machine_is_active) {
-        Stages current_stage = stage;
-        if (current_stage != desired_stage) throw;
-      }
-      else
-      {
-        throw;
-      }
-		
-
+        if (stage != desired_stage) throw;
         _;
     }
 
-    function DeliveryContract(bytes32 _name, bytes32 _code, bool _state_machine, address _foodTokenAddress) {
+    function DeliveryContract(bytes32 _name, bytes32 _code, address _foodTokenAddress) {
         owner = msg.sender;
         name = _name;
         code = _code;
         foodToken = FoodToken(_foodTokenAddress);
-        state_machine_is_active = _state_machine;
-    }
-    
-    function enable_state_machine() onlyOwner
-    {
-      // It can only be turned on and not off once deployed for security reasons.
-      // The only way to turn the state machine off is to deploy it with it off.
-      state_machine_is_active = true;
     }
 
     function setAttributes(bytes32 [] _identifers, int [] _mins, int [] _maxs) onlyOwner onlyStage(Stages.New) {
-      stage = Stages.HasAttributes;
-      if (_identifers.length != _mins.length) throw;
-      if (_identifers.length != _maxs.length) throw;
-      for (uint i = 0; i < _identifers.length; i++) {
-        attributes.push(Attribute(_identifers[i], _mins[i], _maxs[i]));
-      }
+        stage = Stages.HasAttributes;
+        if (_identifers.length != _mins.length) throw;
+        if (_identifers.length != _maxs.length) throw;
+        for (uint i = 0; i < _identifers.length; i++) {
+            attributes.push(Attribute(_identifers[i], _mins[i], _maxs[i]));
+        }
     }
 
-    function inviteParticipants(address [] _parties, uint [] _amounts) onlyOwner onlyStage(Stages.HasAttributes) returns (bool) {
-				stage = Stages.WaitingForParties;
-				escrowed_amount = sum(_amounts);
-				for (uint i = 0; i < _parties.length; i++) {
-					parties.push(Party(_parties[i], _amounts[i], false));
-					party_no += 1;
-				}
+    function inviteParticipants(address [] _parties, uint [] _amounts) onlyOwner onlyStage(Stages.HasAttributes) returns (bool)
+    {
+        stage = Stages.WaitingForParties;
+        escrowed_amount = sum(_amounts);
+        for (uint i = 0; i < _parties.length; i++) {
+            parties.push(Party(_parties[i], _amounts[i], false));
+            party_no += 1;
+        }
 
-				return foodToken.transferFrom(owner, this, escrowed_amount);
+        return foodToken.transferFrom(owner, this, escrowed_amount);
     }
     
-    function processInvite(uint party_index, bool response) onlyStage(Stages.WaitingForParties)
+    function processInvite(uint party_index, bool response) onlyStage(Stages.WaitingForParties) returns (uint)
     {		
-      // Party doesn't exist -- overflow.
-      if(party_index + 1 > party_no)
-      {
-        throw;
-      }
-
-      // Is this the owner of the invite?
-      if(msg.sender != parties[party_index].wallet)
-      {
-        throw;
-      }
-
-      // They've already accepted.
-      if(parties[party_index].has_accepted)
-      {
-        throw;
-      }
-
-      // User has accepted.
-      if(response)
-      {
-        parties[party_index].has_accepted = true;
-        accepted_no += 1;
-
-        // All accepted.
-        if(accepted_no == party_no)
+        // Party doesn't exist -- overflow.
+        if(party_index + 1 > party_no)
         {
-        stage = Stages.InProgress;
+            throw;
         }
-      }
-      else
-      {
-        stage = Stages.Canceled;
-      }
+
+        // Is this the owner of the invite?
+        if(msg.sender != parties[party_index].wallet)
+        {
+            throw;
+        }
+
+        // They've already accepted.
+        if(parties[party_index].has_accepted)
+        {
+            throw;
+        }
+
+        // User has accepted.
+        if(response)
+        {
+            parties[party_index].has_accepted = true;
+            accepted_no += 1;
+
+            // All accepted.
+            if(accepted_no == party_no)
+            {
+                stage = Stages.InProgress;
+            }
+        }
+        else
+        {
+            stage = Stages.Canceled;
+        }
     }
 
     function approve() onlyOwner onlyStage(Stages.InProgress)  {
-      for (uint i = 0; i < parties.length; i++) {
-          foodToken.transfer(parties[i].wallet, parties[i].amount);
-      }
-      stage = Stages.Complete;
+        for (uint i = 0; i < parties.length; i++) {
+            foodToken.transfer(parties[i].wallet, parties[i].amount);
+        }
+        stage = Stages.Complete;
     }
 
-    function reimburse() onlyOwner onlyStage(Stages.WaitingForParties) onlyStage(Stages.Canceled){		
-      if (msg.sender != owner) throw;
-      stage = Stages.Reimbursed;
-      uint amount = escrowed_amount;
-      escrowed_amount = 0;
-      foodToken.transfer(owner, amount);
+    function reimburse() onlyOwner {
+        if (stage != Stages.Canceled && stage != Stages.InProgress && stage != Stages.WaitingForParties) {
+            throw;
+        }
+        
+        if (msg.sender != owner) throw;
+        stage = Stages.Reimbursed;
+        uint amount = escrowed_amount;
+        escrowed_amount = 0;
+        foodToken.transfer(owner, amount);
     }
 
     function addMeasurements(bytes32 [] _events, bytes32 [] _attributes, int [] _values, uint [] _timestamps, bytes32 [] _farmer_codes, bytes32 [] _batch_nos) {
@@ -181,9 +168,9 @@ contract DeliveryContract is Assertive {
         int [] memory mins = new int[](attributes.length);
         int [] memory maxs = new int[](attributes.length);
         for (uint i = 0; i < attributes.length; i++) {
-          identifers[i] = attributes[i].identifer;
-          mins[i] = attributes[i].min;
-          maxs[i] = attributes[i].max;
+            identifers[i] = attributes[i].identifer;
+            mins[i] = attributes[i].min;
+            maxs[i] = attributes[i].max;
         }
         return (identifers, mins, maxs);
     }
@@ -197,13 +184,13 @@ contract DeliveryContract is Assertive {
         bytes32 [] memory farmer_ids = new bytes32[](measurements.length);
         bytes32 [] memory batch_ids = new bytes32[](measurements.length);
         for (uint i = 0; i < measurements.length; i++) {
-          attribute_ids[i] = measurements[i].attribute_id;
-          values[i] = measurements[i].value;
-          event_ids[i] = measurements[i].event_id;
-          timestamps[i] = measurements[i].timestamp;
-          block_timestamps[i] = measurements[i].block_timestamp;
-          farmer_ids[i] = measurements[i].farmer_id;
-          batch_ids[i] = measurements[i].batch_id;
+            attribute_ids[i] = measurements[i].attribute_id;
+            values[i] = measurements[i].value;
+            event_ids[i] = measurements[i].event_id;
+            timestamps[i] = measurements[i].timestamp;
+            block_timestamps[i] = measurements[i].block_timestamp;
+            farmer_ids[i] = measurements[i].farmer_id;
+            batch_ids[i] = measurements[i].batch_id;
         }
         return (event_ids, attribute_ids, values, timestamps, block_timestamps, farmer_ids, batch_ids);
     }
@@ -212,17 +199,17 @@ contract DeliveryContract is Assertive {
         address [] memory wallets = new address[](parties.length);
         uint [] memory amounts = new uint[](parties.length);
         for (uint i = 0; i < parties.length; i++) {
-          wallets[i] = parties[i].wallet;
-          amounts[i] = parties[i].amount;
+            wallets[i] = parties[i].wallet;
+            amounts[i] = parties[i].amount;
         }
         return (wallets, amounts);
     }
 
     function sum(uint[] memory self) internal constant returns (uint r) {
-      r = self[0];
-      for (uint i = 1; i < self.length; i++) {
-        r += self[i];
-      }
+        r = self[0];
+        for (uint i = 1; i < self.length; i++) {
+            r += self[i];
+        }
     }
 
 }
--- test/state_machine.js
+++ test/state_machine.js
@@ -3,7 +3,7 @@
 const DeliveryContract = artifacts.require("./DeliveryContract.sol");
 const FoodToken = artifacts.require("./FoodToken.sol");
 const assert = require('assert');
-//const testutils = require("./testutils.js");
+const testutils = require("./testutils.js");
 const BigNumber = require('bignumber.js');
 
 let delivery;
@@ -11,61 +11,36 @@ let delivery;
 let token;
 
 function setup(accounts) {
-  DeliveryContract.deployed("The Name", "The Code").then((deployed) => delivery = deployed );
+  DeliveryContract.deployed().then((deployed) => delivery = deployed );
   FoodToken.deployed().then((deployed) => token = deployed);
 }
 
 contract('DeliveryContract', function(accounts) {
-  before('Init contracts', (done) => { setup(accounts); done(); });
+    before('Init contracts', (done) => { setup(accounts); done(); });
 
-  it("should listen to state machine", (done) => {
-
-    delivery.enable_state_machine()
-    .then(() => {
-        delivery.state_machine_is_active()
-      .then((result) => {
-        assert.equal(result, true);
-      })
-      .then(() => {
-        // Test New state is enforced.
-        delivery.state_machine_is_active().then((result) => {
-          console.log(result);
-        });
-        delivery.stage().then((result) => {
-          console.log(result["c"][0]);
-        });
+    it("should listen to state machine", (done) => {
         delivery.inviteParticipants([accounts[1], accounts[2]], [33, 67])
         .then(() => {
-          delivery.getParticipants()
-          .then((result) => {
+            return delivery.getParticipants()
+        }).then((result) => {
             assert.equal(result[0].length, 0); // I.E. nothing was found [ [], [] ]
-          });
         })
 
         // Test HasAttribute is enforced.
         .then(() => {
-          return delivery.setAttributes(["Volume", "Color"], [22, 768], [24, 786])
-          .then(() => {
-            delivery.stage().then((result) => {
-              assert.equal(result["c"][0], 1);
-            });
-          })
-          .then(() => {
+            return delivery.setAttributes(["Volume", "Color"], [22, 768], [24, 786])
+        }).then(() => {
+            return delivery.stage();
+        }).then((result) => {
+            assert.equal(result["c"][0], 1);
+        }).then(() => {
             return delivery.inviteParticipants([accounts[1], accounts[2]], [33, 67]);
-          });
-        })
-        .then(() => {
-          return delivery.getParticipants();
-        })
-        .then((result) => {
-          console.log(result);
-        });
+        }).then(() => {
+            return delivery.getParticipants();
+        }).then((result) => {
+            assert.equal(result[0][0], accounts[1]);
+        }).then(done);
 
-        
-
-      });
-    }).then(done);
-
-  });
+    });
 });
 
