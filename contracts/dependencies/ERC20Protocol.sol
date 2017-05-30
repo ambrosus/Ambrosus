6f60f86c8a33988de975156293e5e51c96aad680
Marek Kirejczyk <marekkirejczyk@Mareks-MBP.lan>
2017-05-30 14:43:47 +0200

    remove warnnings

--- contracts/dependencies/ERC20Protocol.sol
+++ contracts/dependencies/ERC20Protocol.sol
@@ -7,10 +7,10 @@ contract ERC20Protocol {
 
     function totalSupply() constant returns (uint256) {}
     function balanceOf(address) constant returns (uint256) {}
-    function transfer(address _to, uint256 _value) returns (bool) {}
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool) {}
-    function approve(address _spender, uint256 _value) returns (bool) {}
-    function allowance(address _owner, address _spender) constant returns (uint256) {}
+    function transfer(address, uint256) returns (bool) {}
+    function transferFrom(address, address, uint256) returns (bool) {}
+    function approve(address, uint256) returns (bool) {}
+    function allowance(address, address) constant returns (uint256) {}
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
