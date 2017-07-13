403c21f98b41500aa3048a4028e1b0a71e3b0e6d
Ivan Rukhavets <ivan@Ivans-MacBook-Pro.local>
2017-07-13 18:31:36 +0200

    added files

---
+++ storage-test/ipfs_map.js
@@ -0,0 +1,27 @@
+const IPFS = require('ipfs');
+const IPFSMap = require("../lib/IPFSMap.js");
+const assert = require('assert')
+
+
+describe('IPFSStorage', function() {
+
+    it("adding to catalog", (done) => {
+        var ipfs = new IPFS();
+        ipfs.on('ready', async () => {
+            var map = await IPFSMap.create(ipfs);
+            var hash1 = await map.add('test');
+            var hash2 = await map.add('test2');
+            var links = map.keys();
+            var data = await map.values();
+
+            assert.equal(links.length, 2);
+            assert.equal(data.length, 2);
+            assert.deepEqual(data.sort(), ['test', 'test2']);
+            assert.deepEqual(links.map((l)=>l.multihash).sort(), [hash1,hash2].sort());
+
+            ipfs.stop(done);
+        });
+        
+    });
+
+});
